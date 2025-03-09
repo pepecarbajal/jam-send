cfe41d30c648e96b85e6921c360c39872b36775a
gv-pepe <prbsPepe@gmail.com>
2025-03-09 13:27:03 -0600

    update server.js

--- server.js
+++ server.js
@@ -7,16 +7,13 @@ const messageRoutes = require('./routes/message');
 const app = express();
 const port = 3000;
 
-// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Conectar a MongoDB
 connectDB();
 
 app.use('/', messageRoutes);
 
-// Iniciar el servidor
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
 });
