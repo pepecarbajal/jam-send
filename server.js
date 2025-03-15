ed6242043e8d9b056ed4d7a4f226e0bbcf85fb85
gv-pepe <prbsPepe@gmail.com>
2025-03-14 19:53:54 -0600

    error cors

--- server.js
+++ server.js
@@ -12,10 +12,18 @@ app.use((req, res, next) => {
     const origin = req.headers.origin;
 
     if (origin && allowedOrigins.includes(origin)) {
-        next();
+        res.header('Access-Control-Allow-Origin', origin);
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+        if (req.method === 'OPTIONS') {
+            return res.status(200).end();
+        }
     } else {
-        res.status(403).json({ message: "Acceso denegado" });
+        return res.status(403).json({ message: "Acceso denegado" });
     }
+
+    next();
 });
 
 app.use(bodyParser.json());
